2b894d4a2a1b714a96db80d828582eb188c41434
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2025-01-11 22:45:59 +0300

    feat: new widget color

--- app/(api)/api/widget/posts/route.tsx
+++ app/(api)/api/widget/posts/route.tsx
@@ -2,16 +2,45 @@ import React from 'react';
 import { ImageResponse } from 'next/og';
 import PostService from '@/services/PostService';
 import { NextRequest } from 'next/server';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 export async function GET(request: NextRequest) {
     const { posts } = await PostService.getAllPosts({ page: 1, pageSize: 10, search: undefined });
 
     // Convert posts to JSX list items
-    const listItems = posts.map((post, index) => (
-        <li key={post.title} style={{ marginTop: index * 20 , position: 'absolute' }}>
-            {post.title}
-        </li>
+    const listItems = posts.slice(0, 10).map((post, index) => (
+        <div key={post.title} style={{
+            fontSize: 15,
+            width: 850,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            padding: '20px',
+            position: 'absolute',
+            top: index * 30 + 40,
+        }}>
+            &#8226; {post.title.length > 100 ? post.title.substring(0, 100) + '...' : post.title}
+        </div>
     ));
+
+    const header = (
+        <div style={{
+            width: 600,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            padding: '20px',
+            position: 'absolute',
+            top: 0,
+            fontWeight: 'bold',
+            fontSize: 20,
+        }}>
+            Latest Posts
+        </div>
+    );
+
+    listItems.unshift(header);
 
     return new ImageResponse(
         (
@@ -24,14 +53,15 @@ export async function GET(request: NextRequest) {
                     flexDirection: 'row',
                     padding: '20px',
                     position: 'relative',
+                    backgroundColor: '#c3c8d7',
                 }}
             >
-                <ul style={{  padding: 0 }}>{listItems}</ul>
+                <ul style={{ padding: 0 }}>{listItems}</ul>
             </div>
         ),
         {
-            width: 1200,
-            height: 600,
+            width: 900,
+            height: 400,
         }
     );
 }
