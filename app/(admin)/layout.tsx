b81729b497f103861e4e8864347d108acc944ead
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2026-02-12 16:01:21 +0300

    fix: i18n

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Suspense, ReactNode } from 'react'
 import 'react-toastify/dist/ReactToastify.css'
+import '@/libs/localize/localize' // i18n initialization
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return <Suspense>{children}</Suspense>
